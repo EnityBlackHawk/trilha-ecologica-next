7a8d2f694af2eaa5a4b08a88f9d2a29782b832b6
Luan Felipe Marmentini <luanf2003@gmail.com>
2024-06-06 15:24:20 -0300

    feat: Add update, delete fauna

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,26 +29,7 @@ classTaxonomica: "Gimnosperma",
 
 */
 
-const objPlantInfo: PlantInfo = {
-  classTaxonomica: { value: "", name: "Class Taxonomica" },
-  familiaBotanica: { value: "", name: "Familia Botanica" },
-  grauAmeaca: { value: "", name: "Grau Ameaca" },
-  nomePopular: { value: "", name: "Nome Popular" },
-  nomeCientifico: { value: "", name: "Nome Cientifico" },
-  biomaEstados: { value: "", name: "Bioma Estados" },
-  extratoVertical: { value: "", name: "Extrato Vertical" },
-  populacao: { value: "", name: "Populacao" },
-  DAP: { value: "", name: "DAP" },
-  solos: { value: "", name: "Solos" },
-  idadeMaxima: { value: "", name: "Idade Maxima" },
-  altitude: { value: "", name: "Altitude" },
-  dispersores: { value: "", name: "Dispersores" },
-  estagio: { value: "", name: "Estagio" },
-  produtos: { value: "", name: "Produtos" },
-};
 
 export default function Home() {
-  const [inputData, setInputData] = useState<PlantInfo>(objPlantInfo);
-  const [test, setTest] = useState<string>("");
   return (<div></div>)
 }
