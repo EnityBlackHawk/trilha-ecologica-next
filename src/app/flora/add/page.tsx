4a222edc0d431eec38388e04f2a5a060bebeaeef
Luan Felipe Marmentini <luanf2003@gmail.com>
2024-06-06 13:46:41 -0300

    feat: Add save fauna

--- src/app/flora/add/page.tsx
+++ src/app/flora/add/page.tsx
@@ -18,25 +18,6 @@ import { useSearchParams } from "next/navigation";
 import { PlantInfoWithImage } from "@/app/api/flora/route";
 import { useRouter } from "next/navigation";
 
-/* 
-
-classTaxonomica: "Gimnosperma",
-    familiaBotanica: "Araucariaceae",
-    grauAmeaca: "EN",
-    nomePopular: "Araucária - Pinheiro do Paraná",
-    nomeCientifico: "Araucaria angustifolia (Bertol.) Kuntze",
-    biomaEstados: "FOM (RS-PR)",
-    extratoVertical: "DOSSEL (30m)",
-    populacao: "ABUNDANTE",
-    DAP: "50-60 cm",
-    solos: "SOLOS BEM DRENADOS",
-    idadeMaxima: "300 anos",
-    altitude: "500m - 1200m",
-    dispersores: "Aves-Mamíferos",
-    estagio: "PIONEIRA-MÉDIO",
-    produtos: "MADEIRA, FRUTO",
-
-*/
 
 const objPlantInfo: PlantInfo = {
   nomeCientifico: { value: "", name: "Nome Cientifico" },
--- src/types.ts
+++ src/types.ts
@@ -13,3 +13,18 @@ export interface PlantInfo {
   polinizacao : { value: string; name: string };
   gs: { value: string; name: string };
 };
+
+export interface AnimalInfo {
+  nomeCientifico: { value: string; name: string };
+  grauAmeaca: { value: string; name: string };
+  nomePopular: { value: string; name: string };
+  bioma: { value: string; name: string };
+  peso: { value: string; name: string };
+  comprimento: { value: string; name: string };
+  habitat : { value: string; name: string };
+  alimentacao : { value: string; name: string };
+  habitos : { value: string; name: string };
+  territorio: { value: string; name: string };
+  reproducao : { value: string; name: string };
+  longevidade : { value: string; name: string };
+}
